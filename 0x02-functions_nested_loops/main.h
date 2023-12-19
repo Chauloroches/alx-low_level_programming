88cd542fc2a45710599d5418c5b3e30a71b41477
Chauloroches <rocjeschaulo@gmail.com>
2023-12-19 06:20:36 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-int_putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,5 +9,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);	
-#endif
+void print_times_table(int n);
+int _putchar(int c);
