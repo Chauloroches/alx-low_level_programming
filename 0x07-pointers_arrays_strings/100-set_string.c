487a430ad3624eece0ba79c0641068b94e8dcd60
Chauloroches <rocjeschaulo@gmail.com>
2024-01-08 04:33:03 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * set_string - contains the password for the crackme2 executable.
+  * @s: parameter
+  * @to: parameter
+  * Return: 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
+
