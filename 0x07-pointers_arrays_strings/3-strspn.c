c7ef337d184ad41df40df25a5f305e072954690b
Chauloroches <rocjeschaulo@gmail.com>
2024-01-08 05:25:03 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,30 @@
 #include "main.h"
 
 /**
-  * _strspn - main function
-  * @s: parameter
-  * @accept: parameter
-  * Return: 0.
-  */
+ * _strspn - main function
+ * @s: parameter
+ * @accept: parameter
+ * Return: 0.
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-
-	unsigned int _strspn(char *s, char *accept);
 	unsigned int a = 0;
+
 	int b;
 
 	while (*s)
 	{
-	for (a = 0; accept[b]; b++)
+	for (b = 0; accept[b]; b++)
 	{
 	if (*s == accept[b])
 	{
-	a++;
+		a++;
 	break;
 	}
 	else if (accept[b + 1] == '\0')
-	return (a);
+		return (a);
 	}
 	s++;
 	}
 	return (a);
 }
-
