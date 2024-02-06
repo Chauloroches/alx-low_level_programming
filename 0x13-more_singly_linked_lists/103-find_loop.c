fc068513288f0cea8b119fd0579aace96bd5ff47
Chauloroches <rocjeschaulo@gmail.com>
2024-02-06 13:14:57 -0800

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,8 +2,7 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
- *
+ * @head: pointer
  * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
