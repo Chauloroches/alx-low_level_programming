63adb3289a29d8c1b523fab947fdeced5cb34d00
Chauloroches <rocjeschaulo@gmail.com>
2024-02-06 03:45:52 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,20 +2,18 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
+ * @h: parameter
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-size_t num = 0;
+size_t i = 0;
 
-while (h)
+while (h != NULL)
 {
-num++;
 h = h->next;
+i++;
 }
 
-return (num);
+return (i);
 }
-
