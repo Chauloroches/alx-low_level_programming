159625fe047d121fb3e9d246fa94464b31943883
Chauloroches <rocjeschaulo@gmail.com>
2024-02-06 04:44:28 -0800

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_listint -  frees a listint_t list
+ * @head: parameter
  */
 void free_listint(listint_t *head)
 {
