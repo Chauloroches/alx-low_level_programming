4ef17193bcf2dbccb1b8d4965d138ebf93103828
Chauloroches <rocjeschaulo@gmail.com>
2024-07-20 10:45:23 -0700

    gcc -Wall -Wextra -Werror -pedantic -std=gnu89 103-main.c 103-exponential.c -o 103-exponential

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,16 @@
 #include <math.h>
-#include "103-exponential.h"
+#include "search_algos.h"
 
-int binary_search(int *array, int left, int right, int value);
+/**
+ * exponential_search - Function that searches for a value in a sorted
+ * array of integers
+ * using the Exponential search algorithm
+ * array -  Pointer to the first element of the array to search in
+ * size -  The number of elements in array
+ * value -  The value to search for
+ * Return - If value is not present in array or if array is NULL
+ * your function must return -1
+*/
 
 int exponential_search(int *array, size_t size, int value)
 {
@@ -19,8 +28,14 @@ int exponential_search(int *array, size_t size, int value)
 
 	return (binary_search(array, i / 2, (i < size ? i : size - 1), value));
 }
+
 /**
- * binary_search - Function to search in binary
+ * binary_search - Search for binary
+ * array - pointer parameter
+ * left - parameter
+ * right - parameter
+ * value - parameter
+ * Return - Result -1
 */
 int binary_search(int *array, int left, int right, int value)
 {
@@ -35,7 +50,7 @@ int binary_search(int *array, int left, int right, int value)
 	if (array[mid] < value)
 		left = mid + 1;
 	else
-	right = mid - 1;
+		right = mid - 1;
 	}
 	return (-1);
 }
